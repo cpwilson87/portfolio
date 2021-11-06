8eefe5618f00f98d0c07f17edfc72456a7e25660
Chris Wilson <cpwilson87@outlook.com>
2021-11-06 11:55:03 +0000

    feat: add navbar and routing

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,8 +2,11 @@
 	<title>CP Wilson | Home</title>
 </svelte:head>
 
-<h1>Welcome to My Portfolio Site</h1>
+<h1>Hi All, I'm Chris</h1>
 
 <p>
-	This will be the home page where we make everyone feel welcome and glad they visited the site.
+	A passionate Full Stack Web Developer with experience building Web applications with JavaScript,
+	React, Svelte and Node.
 </p>
+
+<p>I love exploring new technologies and staying on top of the latest trends.</p>
