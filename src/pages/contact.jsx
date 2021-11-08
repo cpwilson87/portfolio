3ee95fd7d1c2e3ec4e80962047b98d91bdb9dd7a
Chris Wilson <cpwilson87@outlook.com>
2021-11-08 06:38:36 +0000

    feat: add form component to contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,8 +1,57 @@
+import { useState } from 'react';
+
 const Contact = () => {
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [message, setMessage] = useState('');
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log('submit form');
+		console.log({ name, email, message });
+	};
 	return (
-		<div className="">
+		<section>
 			<h1>Contact</h1>
-		</div>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, accusamus minus. Repellendus
+				doloremque iure error eaque eligendi recusandae optio iste?
+			</p>
+			<form onSubmit={handleSubmit}>
+				{/* Name */}
+				<label htmlFor="name">Name:</label>
+				<input
+					type="text"
+					required
+					placeholder="Enter Your Name"
+					value={name}
+					id="name"
+					onChange={(e) => setName(e.target.value)}
+				/>
+
+				{/* Email */}
+				<label htmlFor="email">Email:</label>
+				<input
+					type="email"
+					required
+					placeholder="Enter Your Email"
+					value={email}
+					id="email"
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+				{/* Message */}
+				<label htmlFor="message">Message:</label>
+				<textarea
+					required
+					placeholder="Enter Your Message"
+					value={message}
+					id="message"
+					onChange={(e) => setMessage(e.target.value)}
+				/>
+				{/* Submit */}
+				<button type="submit">Submit</button>
+			</form>
+		</section>
 	);
 };
 export default Contact;
