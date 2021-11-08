4efd2af5eeafb73b055c2229a75de966224c013c
Chris Wilson <cpwilson87@outlook.com>
2021-11-08 11:45:48 +0000

    feat: add home page content

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,13 @@
+import styles from '@/styles/home.module.css';
+
 const Home = () => {
 	return (
-		<div className="">
-			<h1>Home </h1>
-		</div>
+		<section className={styles.container}>
+			<p className="text-green font-mono">Hi, my name is</p>
+			<p className="h1">Chris Wilson.</p>
+			<p className="h1 text-secondary">I build things for the web.</p>
+			<p>I am a software engineer specialising in building exceptional digital experiences.</p>
+		</section>
 	);
 };
 export default Home;
