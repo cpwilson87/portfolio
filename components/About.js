4a5cf789ed8141548dea2d3759af2ab9751613bb
Chris Wilson <cpwilson87@outlook.com>
2021-11-18 11:45:28 +0000

    feat: add about section

--- components/About.js
+++ components/About.js
@@ -1,7 +1,26 @@
 export default function About() {
 	return (
 		<section id="about">
-			<h1>About</h1>
+			<h1 className="subtitle">About Me</h1>
+			<p>
+				Hello! My name is Chris and I enjoy designing and creating solutions to help make the world
+				a little easier. I love to make front end applications that combine nice clean interfaces
+				with great user experiences.
+			</p>
+			<p>I am new to the world of web development and loving the challenges it presents.</p>
+			<p>
+				I am currently looking for my first full-time position as a web developer and would love to
+				hear from you. If you are interested in hiring me please <a href="#contact">contact me.</a>
+			</p>
+
+			<p>Here are a few technologies I have been working with recently:</p>
+			<ul>
+				<li>JavaScript (ES6+)</li>
+				<li>TypeScript</li>
+				<li>React</li>
+				<li>Svelte</li>
+				<li>Node.js</li>
+			</ul>
 		</section>
 	);
 }
