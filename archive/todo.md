672da3da7415fd68bcb254f89d3df8b821fcb4ac
Chris Wilson <cpwilson87@outlook.com>
2021-11-18 12:51:49 +0000

    feat: create projects section with cards

--- archive/todo.md
+++
@@ -1,1 +0,0 @@
-- add head meta data
--- components/Projects.js
+++ components/Projects.js
@@ -1,7 +1,72 @@
+const projects = [
+	{
+		id: 1,
+		link: '#',
+		title: 'Project 1',
+		description:
+			'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime libero modi et eaque omnis! Assumenda architecto obcaecati dicta molestias? Natus.',
+		tags: ['React', 'Tailwind']
+	},
+	{
+		id: 2,
+		link: '#',
+		title: 'Project 2',
+		description:
+			'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime libero modi et eaque omnis! Assumenda architecto obcaecati dicta molestias? Natus.',
+		tags: ['React', 'Tailwind']
+	},
+	{
+		id: 3,
+		link: '#',
+		title: 'Project 3',
+		description:
+			'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime libero modi et eaque omnis! Assumenda architecto obcaecati dicta molestias? Natus.',
+		tags: ['React', 'Tailwind']
+	},
+	{
+		id: 4,
+		link: '#',
+		title: 'Project 4',
+		description:
+			'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime libero modi et eaque omnis! Assumenda architecto obcaecati dicta molestias? Natus.',
+		tags: ['React', 'Tailwind']
+	},
+	{
+		id: 5,
+		link: '#',
+		title: 'Project 5',
+		description:
+			'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime libero modi et eaque omnis! Assumenda architecto obcaecati dicta molestias? Natus.',
+		tags: ['React', 'Tailwind']
+	},
+	{
+		id: 6,
+		link: '#',
+		title: 'Project 6',
+		description:
+			'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime libero modi et eaque omnis! Assumenda architecto obcaecati dicta molestias? Natus.',
+		tags: ['React', 'Tailwind']
+	}
+];
+
 export default function Projects() {
 	return (
 		<section id="projects">
-			<h1>Projects</h1>
+			<h2 className="subtitle">Projects</h2>
+			<div className="grid">
+				{projects.map((project) => (
+					<article key={project.id}>
+						<header>
+							<h3 className="h4 text-green">{project.title}</h3>
+						</header>
+						<p>{project.description}</p>
+						<footer>
+							<small>React</small>
+							<small>Tailwind</small>
+						</footer>
+					</article>
+				))}
+			</div>
 		</section>
 	);
 }
